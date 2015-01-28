6532545a5a1831af379215aacde67f25be1a6151
sebdoesntknow <seba.grillo@gmail.com>
2015-01-28 18:49:31 -0200

    ignore file update

--- app/api_search/forms.py
+++ app/api_search/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 class ApiSearchForm(Form):
-    search_bar = StringField('Type here...')
+    search_bar = StringField('What are we looking for?')
     submit = SubmitField('Go!')
