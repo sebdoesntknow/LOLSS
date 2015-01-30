d43ce3be133e34bb764606273cfd5eececf884cc
sebdoesntknow <seba.grillo@gmail.com>
2015-01-30 01:13:54 -0200

    api tools update: ApiGetter class

--- app/tools/riot_api_tools.py
+++ app/tools/riot_api_tools.py
@@ -1,7 +1,24 @@
-import requests
+import os, requests
 
-def query_riot_api(term):
-    riot_api = "https://api.twitch.tv/kraken/search/streams?q={}"
-    url = riot_api.format(term)
-    result = requests.get(url).json()
-    return result
+class ApiGetter(object):
+
+    def __init__(self):
+
+        # Riot Games API
+        self.riot = "RITO PLS"
+        # API Dev Key
+        self.riot_dev_key = os.environ.get('RIOT_DEV_KEY')
+        # Champions
+        self.riot_champs = os.environ.get('RIOT_API_CHAMP_INFO')
+        # Other data below
+
+        # Twitch.tv API
+        self.twitch = "Kaceytron lel"
+        # API Search engine
+        self.twitch_api = "https://api.twitch.tv/kraken/search/streams?q={}"
+
+    def get_riot(self):
+        pass
+
+    def get_twitch(self, search):
+        yield requests.get(self.twitch_api.format(search)).json()
