065753f0a2bb01b97ac4f08f468b42fcc6266006
sebdoesntknow <seba.grillo@gmail.com>
2015-05-06 21:11:03 -0300

    cleaning up config

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
-#from flask.ext.mail import Mail
-#from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from werkzeug.contrib.fixers import ProxyFix
 from config import config
 
 bootstrap = Bootstrap()
-#mail = Mail()
-#moment = Moment()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -25,8 +21,6 @@ def create_app(config_name):
 
     login_manager.init_app(app)
     bootstrap.init_app(app)
-    #mail.init_app(app)
-    #moment.init_app(app)
     db.init_app(app)
 
     # attach routes and custom error pages here
